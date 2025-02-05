f8daba7db34ef7a532967ead5d1e48bbd674f839
Aurora <aurorainge@gmail.com>
2025-02-05 14:50:18 +0100

    file to create subset

---
+++ create_subsets.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+source_dir = "data/masked_annotations/"
+dest_dir = "data/masked_annotations_subset/"
+
+# List of files to copy
+subset = [
+    "train_0.tif", "train_1.tif", "train_2.tif" "train_3.tif", "train_4.tif", 
+    "train_5.tif", "train_6.tif", "train_7.tif", "train_8.tif", 
+    "train_9.tif", "train_10.tif"
+]
+
+os.makedirs(dest_dir, exist_ok=True)
+
+# Copy the files
+for file in os.listdir(source_dir):
+    if file in subset:
+        file_path = os.path.join(source_dir, file)
+        dest_path = os.path.join(dest_dir, file)
+        shutil.copy(file_path, dest_path)
+        print(f"Copied {file} to {dest_dir}")
+
+print("File copying completed.")
