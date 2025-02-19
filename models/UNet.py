2eab2f9ef9fdefbd5ffbc17e067a2fb041715166
synneremmen <91556819+synneremmen@users.noreply.github.com>
2025-02-19 16:32:32 +0100

    implemented UNet (working)

--- models/UNet.py
+++ models/UNet.py
@@ -2,37 +2,89 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-class SimpleConvNet(nn.Module):
+class encoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(encoder, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class decoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(decoder, self).__init__()
+
+        self.conv_transpose = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels + out_channels, in_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+        
+    def forward(self, x, skip_features):
+        x = self.conv_transpose(x)
+        skip_features = F.interpolate(skip_features, size=(x.shape[2], x.shape[3]), mode='bilinear', align_corners=False)
+        x = torch.cat([x, skip_features], dim=1) # applying skip connection
+        x = self.conv(x)
+        return x
+
+
+class UNet(nn.Module):
     def __init__(self):
-        super(SimpleConvNet, self).__init__()
+        super(UNet, self).__init__()
         
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.encoder1 = self.encoder(3, 64)
-        self.encoder2 = self.encoder(64, 128)
-        self.encoder3 = self.encoder(128, 256)
+        self.enc1 = encoder(12, 32)
+        self.enc2 = encoder(32, 64)
+        self.enc3 = encoder(64, 128)
+        self.enc4 = encoder(128, 256)
+        
+        self.middle = nn.Sequential(
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 256, 3, padding=1),
+            nn.ReLU()
+        )
 
-    def encoder(self, x, in_channels, num_filters):
-        x = nn.Conv2d(in_channels, num_filters, 3)(x)
-        x = self.relu(x)
-        x = nn.Conv2d(num_filters, num_filters, 3)(x)
-        x = self.relu(x)
-        x = self.pool(x)
-        return x
-    
-    def decoder(self, x, skip_feature, in_channels, num_filters):
-        x = nn.ConvTranspose2d(in_channels, num_filters,kernel_size=(2,2), stride=2)(x)
-        skip_feature = F.interpolate(skip_feature, size=(x.shape[1], x.shape[2]))
-        x = torch.cat((x, skip_feature))
-        x = nn.Conv2d(num_filters, kernel_size=3)(x)
-        x = self.relu(x)
-        x = nn.Conv2d(num_filters, kernel_size=3)(x)
-        x = self.relu(x)
-        return x
+        self.dec1 = decoder(256, 128)
+        self.dec2 = decoder(128, 64)
+        self.dec3 = decoder(64, 32)
+        self.dec4 = decoder(32, 12)
+
+        self.out = nn.Sequential(
+            nn.Conv2d(12, 5, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+
 
     def forward(self, x):
+        # Encoder 
         # [batch, 12, 1024, 1024]
+        e1 = self.enc1(x)
+        e2 = self.enc2(e1)
+        e3 = self.enc3(e2)
+        e4 = self.enc4(e3) 
 
-        # [batch, 5, 1024, 1024]
-        return x
+        # A few layers after encoding
+        x = self.middle(e4)
+
+        # Decoder 
+        x = self.dec1(x, e4) 
+        x = self.dec2(x, e3) 
+        x = self.dec3(x, e2) 
+        x = self.dec4(x, e1)
+        
+        x = self.out(x) # får se om vi trenger eller kaster denne
+        return x # [batch, 5, 1024, 1024]
