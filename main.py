df97ef527df66f8dedf82f5dbb3305ab55bad608
Christian Bontveit <chrizbont@gmail.com>
2025-02-21 15:05:25 +0100

    save model, use model

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import torch.nn as nn
 from train.loader import get_loader
 from utils.postprocessing import run_evaluation
 from torch.utils.data import DataLoader
+import os
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_default_dtype(torch.double)
 
+# model path
+MODEL_PATH = "models/saved_model.pth"
+
 def main():
-    subset = False
+    subset = True
 
     print("\n\nLoading data...\n\n")
     dataset = get_processed_data(subset=subset)
@@ -23,15 +27,23 @@ def main():
     print("Size of test dataset: ", len(test_loader.dataset))
 
     model = SimpleConvNet() # UNet() if you want to use the UNet model
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    loss_fn = nn.CrossEntropyLoss()
-    n_epochs = 30
 
-    print("\n\nTraining model...\n\n")
-    losses_train = train(n_epochs, optimizer, model, loss_fn, train_loader, device=DEVICE)
+    if os.path.exists(MODEL_PATH):
+        print("\n\nLoading saved model...\n\n")
+        model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
+    else:
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+        loss_fn = nn.CrossEntropyLoss()
+        n_epochs = 10
 
-    print("\n\nTraining completed. Training losses:\n\n")
-    print(losses_train)
+        print("\n\nTraining model...\n\n")
+        losses_train = train(n_epochs, optimizer, model, loss_fn, train_loader, device=DEVICE)
+
+        print("\n\nTraining completed. Training losses:\n\n")
+        print(losses_train)
+
+        print("\n\nSaving model...\n\n")
+        torch.save(model.state_dict(), MODEL_PATH)
 
     print("\n\nRunning evaluation...\n\n")
     eval_dataset = get_processed_evaluation_data(subset=subset)
