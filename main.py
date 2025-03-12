1908cc76d6181d8975201a05666dfb2b9848538a
synneremmen <91556819+synneremmen@users.noreply.github.com>
2025-03-12 19:07:02 +0100

    adding model to main, prettified (no need to load data when loading model)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from utils.preprocessing import get_processed_data, get_processed_evaluation_data
 from train.train import train
 import torch
-from models.simple_convnet import SimpleConvNet
-from models.UNet import UNet
+# from models.simple_convnet import SimpleConvNet
+# from models.UNet import UNet
+# from models.UNetLight import UNet_Light
+from models.Resnet import UNetResNet18
 import torch.nn as nn
 from train.loader import get_loader
 from utils.evaluation import run_evaluation
@@ -13,43 +15,43 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_default_dtype(torch.double)
 
 # model path
-MODEL_PATH = "models/saved_model.pth"
+MODEL_PATH = "models/test_saved_model.pth"
 
 def main():
-    subset = False
-
-    print("\n\nLoading data...\n\n")
-    dataset = get_processed_data(subset=subset)
-    train_loader, val_loader, test_loader = get_loader(dataset, batch_size=10)
-
-    print("Size of training dataset: ", len(train_loader.dataset))
-    print("Size of validation dataset: ", len(val_loader.dataset))
-    print("Size of test dataset: ", len(test_loader.dataset))
-
-    model = SimpleConvNet() # UNet() if you want to use the UNet model
+    subset = True
+    model = UNetResNet18().to(DEVICE)# UNet_Light().to(DEVICE) # SimpleConvNet().to(DEVICE) # UNet().to(DEVICE) # UNet_Light().to(DEVICE)
 
     if os.path.exists(MODEL_PATH):
-        print("\n\nLoading saved model...\n\n")
+        print("\n\nLoading saved model...")
         model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
     else:
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+        print("\n\nLoading data...")
+        dataset = get_processed_data(subset=subset)
+        train_loader, val_loader, test_loader = get_loader(dataset, batch_size=10)
+
+        print("Size of training dataset: ", len(train_loader.dataset))
+        print("Size of validation dataset: ", len(val_loader.dataset))
+        print("Size of test dataset: ", len(test_loader.dataset))
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # burde kanskje ha mindre lr når man bruker pretrained resnet?
         loss_fn = nn.CrossEntropyLoss()
         n_epochs = 30
 
-        print("\n\nTraining model...\n\n")
+        print("\n\nTraining model...")
         losses_train = train(n_epochs, optimizer, model, loss_fn, train_loader, device=DEVICE)
 
-        print("\n\nTraining completed. Training losses:\n\n")
+        print("\n\nTraining completed. Training losses:")
         print(losses_train)
 
-        print("\n\nSaving model...\n\n")
+        print("\n\nSaving model...")
         torch.save(model.state_dict(), MODEL_PATH)
 
-    print("\n\nRunning evaluation...\n\n")
+    print("\n\nRunning evaluation...")
     eval_dataset = get_processed_evaluation_data(subset=subset)
     eval_loader = DataLoader(eval_dataset, batch_size=10, shuffle=False)
     torch.cuda.empty_cache()
     run_evaluation(model, eval_loader, device=DEVICE)
+    print("\n\nEvaluation completed.\n\n")
 
 if __name__ == "__main__":
     main()
