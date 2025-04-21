e54703e2ef4db72989b5c38f25bbca0d7f1cceee
Christian Bontveit <chrizbont@gmail.com>
2025-04-21 15:42:55 +0200

    selects best on validation

--- train/selection.py
+++ train/selection.py
@@ -5,32 +5,21 @@ from train import train
 from utils.evaluation import run_evaluation
 from itertools import product
 
-def selection(models:list[nn.Module], params:dict, train_loader, val_loader, device) -> None:
+def selection(models:list[nn.Module], val_loader, device) -> None:
 
     best_model = None
-    best_hyperparams = None
     best_score = float('-inf')
-    loss_fn = nn.CrossEntropyLoss()
 
     for model in models:
-        for n_epochs, optimizer, lr, batch_size in product(*params.values()):
-            hyperparams = {
-                "n_epochs": n_epochs,
-                "optimizer": optimizer,
-                "lr": lr,
-                "batch_size": batch_size
-            }
+        # Compute validation score using `evaluate()`
+        val_score = run_evaluation(model, val_loader, device, save=False)
 
-            model, train_loss, val_loss = train(n_epochs, optimizer, model, loss_fn, train_loader, device, val_loader=None)
-
-             # Compute validation score using `evaluate()`
-            val_score = run_evaluation(model, val_loader, device, save=False)
-
-            print(f"Model: {type(model).__name__}, LR: {lr}, Epochs: {n_epochs}, Val Loss: {val_score}")
+        print(f"Model: {model}, validation score: {val_score}")
     
-            if val_score > best_score:
-                best_score = val_score
-                best_model = model
-                best_hyperparams = hyperparams
+        if val_score > best_score:
+            best_score = val_score
+            best_model = model
+    
+    print(f"Best model: {best_model}, score: {best_score}")
 
-    return best_model, best_hyperparams, best_score
+    return best_model, best_score
