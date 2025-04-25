b37aa1c8170b24906a6bfcddc5db3b309725f6a6
synneremmen <91556819+synneremmen@users.noreply.github.com>
2025-04-25 12:26:58 +0200

    prettify

--- utils/new_individual_object_functions.py
+++ utils/new_individual_object_functions.py
@@ -52,12 +52,11 @@ class Generator:
         self.geometric_augm_prob = 0.5
         self.color_augm_prob = 0
 
-        # self.stands_id = False # vet ikke hva denne gjør. Hvilke id?
         self.batch_size = batch_size
 
         self.channels_background = ['Aerosols', 'Blue', 'Green', 'Red', 'Red Edge 1', 'Red Edge 2', 'Red Edge 3', 'NIR', 'Red Edge 4', 'Water vapor', 'SWIR1', 'SWIR2']
 
-        self.extracted_objects = self.extract_objects(images=images, min_area=min_area, subset=subset)
+        self.extracted_objects = self._extract_objects(images=images, min_area=min_area, subset=subset)
         self.num_of_extracted_objects = len(self.extracted_objects)
 
 
@@ -66,18 +65,16 @@ class Generator:
         Visualizes the image and mask.
         Args:
             image (numpy.ndarray): The image to visualize.
-            mask (numpy.ndarray): The mask to visualize.
-            original_image (numpy.ndarray): The original image to visualize.
-            original_mask (numpy.ndarray): The original mask to visualize.
+            mask (numpy.ndarray, optional): The mask to visualize. Defaults to None.
         """
         fontsize = 18
 
         if mask is None:
-            f, ax = plt.subplots(1, 1, figsize=(8, 8))
+            _, ax = plt.subplots(1, 1, figsize=(8, 8))
             ax.imshow(image)
             
         else:
-            f, ax = plt.subplots(2, 1, figsize=(8, 8))
+            _, ax = plt.subplots(2, 1, figsize=(8, 8))
 
             ax[0, 0].imshow(image)
             ax[0, 0].set_title('Image', fontsize=fontsize)
@@ -88,14 +85,15 @@ class Generator:
         plt.show()
 
 
-    def extract_objects(self, images=None, min_area=100, subset=False): # ikke laste opp alle bildene for å unngå å ta opp så mye plass?
+    def _extract_objects(self, images=None, min_area=100, subset=False): # ikke laste opp alle bildene for å unngå å ta opp så mye plass?
         """
         Creates object-based augmented data by rasterizing the object masks.
         Returns:
             list: A list of tuples containing image names, masks, and object IDs.
         """
-        # load labels with masks for objects
+        # load labels with masks for each objects
         labels = load_labels(subset=subset, object_based_augmentation=True)
+        
         if images is None:
             images = load_images(subset=subset)
 
@@ -127,20 +125,7 @@ class Generator:
         print(f"Extracted {len(all_object_masks)} objects from {len(images)} images.")
         return all_object_masks
 
-
-    def split_dataset(self, images, masks, test_size=0.2) -> list:
-        """
-        Splits the dataset into training and testing sets.
-        Args:
-            images (list): List of images.
-            masks (list): List of masks.
-            test_size (float): Proportion of the dataset to include in the test split.
-        Returns:
-            list: A list containing the training and testing datasets.
-        """
-        return train_test_split(images, masks, test_size=test_size, random_state=42)
-
-    def crop_target_object(self, object_id: int):
+    def _crop_target_object(self, object_id: int):
         """
         Crops the target object from the image and mask based on the object ID.
         Args:
@@ -162,18 +147,170 @@ class Generator:
         with rasterio.open(image_path) as src:
             image = src.read()
 
-        # crop the target object
+        # crop the target object from image
         cropped_object = np.zeros_like(image)
-        #mask = np.where(mask > 0, mask, 0)
-
         for c in range(image.shape[0]):
             cropped_object[c] = np.where(mask > 0, image[c], 0)
 
-        # return target object and mask
         return cropped_object, mask # (12, 1024, 1024), (1024, 1024)
+
+    def _find_background(self):
+        """
+        Finds a random background image and its corresponding mask.
+        Returns:
+            tuple: The background image, mask, and the name of the image.
+        """
+        if len(self.background_list) and random.random() < self.extra_background_prob:
+            print("I should no be in here...")
+            # TODO: not implemented extra backgrounds
+            rnd_choice = random.choice(self.background_list)
+        else:
+            rnd_choice = random.choice(self.file_name_list)
+            with rasterio.open(IMAGES_PATH + "/" + rnd_choice) as src:
+                background = src.read()
+                background = np.nan_to_num(background, nan=0.0)
+
+            mask = self.masks[rnd_choice]["image"][0] # mask has shape (1, 1024, 1024)
+        return background, mask, rnd_choice
+    
+    def _prepare_image_and_mask(self, background, mask):
+        """
+        Prepares the image and mask for augmentation.
+        Args:
+            background (numpy.ndarray): The background image.
+            mask (numpy.ndarray): The mask of the background.
+        Returns:
+            tuple: The prepared image and mask.
+        """
+        background = background.clone() if torch.is_tensor(background) else background.copy()
+        mask  = mask.clone() if torch.is_tensor(mask) else mask.copy()
+        background = background.numpy() if torch.is_tensor(background) else background
+        mask  = mask.numpy() if torch.is_tensor(mask) else mask
+        return background, mask
+
+    def _apply_object_augmentation(self, name, background, mask):
+        """
+        Applies object-based augmentation by adding extra objects to the background.
+        Args:
+            name (str): The name of the image.
+            background (numpy.ndarray): The background image.
+            mask (numpy.ndarray): The mask of the background.
+        Returns:
+            tuple: The augmented image and mask.
+        """
+        temp_mask = mask.copy()
+        temp_image = np.zeros_like(background)
+        target_ids = [obj[-1] for obj in self.extracted_objects if obj[0] == name]
+        num_of_objects = 0
+
+        if self.extra_objects > 0:
+            print("Adding extra objects")
+            random_num = random.randint(0, self.extra_objects)
+            print(f"Random number of objects: {random_num}")
+            for _ in range(random_num):
+                intersection = True
+                iteration = 0
+
+                while intersection and iteration < 10: # iteration: how mmany attempts to try to add an object
+                    iteration += 1
+
+                    extra_obj = random.choice(self.extracted_objects)
+                    extra_id = extra_obj[-1]
+
+                    if extra_id in target_ids:
+                        # do not duplicate objects
+                        continue
+
+                    cropped_obj, cropped_mask = self._crop_target_object(extra_id)
+
+                    if np.sum(cropped_mask * temp_mask) == 0:
+                        if random.random() < self.object_augm_prob:
+                            # by some probability, augment object
+                            cropped_obj, cropped_mask = self._augment_image_mask(cropped_obj, cropped_mask)
+                            self.flag_object_augm = True
+                            if np.sum(cropped_mask * temp_mask) != 0:
+                                # ensure it does not overlap withany objects
+                                continue
+
+                        intersection = False
+                        temp_mask = np.maximum(cropped_mask, temp_mask)
+                        for c in range(temp_image.shape[0]):
+                            temp_image[c] = np.where(cropped_mask > 0, cropped_obj[c], temp_image[c])
+
+                        target_ids.append(extra_id)
+                        num_of_objects += 1
+            print(f"Added {num_of_objects} objects")
+            temp_mask = np.where(mask > 0, 0, temp_mask)
+        # else:
+        #     # should be an object selected, not image. So this should be changed
+        #     cropped_obj, cropped_mask = self._crop_target_object(target_ids[0]) 
+        return temp_image, temp_mask # returns image and mask of cropped objects
+
+    
+    def _add_shadows(self, img, mask):
+        """
+        Adds shadows to the image based on the mask.
+        Args:
+            img (numpy.ndarray): The image to add shadows to.
+            mask (numpy.ndarray): The mask indicating where to add shadows.
+        Returns:
+            numpy.ndarray: The image with shadows added.
+        """
+        if mask.ndim == 3:
+            mask = mask[0] # ensure correct dimensions
+
+        shadow_mask = mask.copy()
+
+        shift_x = random.randint(0, 4)
+        shift_y = random.randint(0, 4)
+
+        shifted_mask = np.zeros_like(shadow_mask)
+        h, w = shadow_mask.shape
+        shifted_mask[shift_x:, shift_y:] = shadow_mask[:h - shift_x, :w - shift_y]
+
+        shadow = (shifted_mask > 0) & (mask == 0)
+        
+        alpha = random.choice([0.4, 0.5]) #([0.3, 0.4, 0.45])
+        for idx, _ in enumerate(self.channels_background):
+            img[idx] = np.where(shadow > 0, img[idx] * alpha, img[idx])
+
+        return img # returns objects with shadows
+
+    def _augment_image_mask(self, image, mask):
+        """
+        Augments the image and mask using the specified augmentation probabilities.
+        Args:
+            image (numpy.ndarray): The image to augment.
+            mask (numpy.ndarray): The mask to augment.
+        Returns:
+            tuple: The augmented image and mask.
+        """
+        augm_image, augm_mask  = augment(image, mask, self.augm_prob, self.color_augm_prob, self.geometric_augm_prob)
+        return augm_image, augm_mask
+    
+    def _insert_objects_to_background(self, objects_image, objects_mask, background, mask):
+        """
+        Inserts the objects into the background image and mask.
+        Args:
+            objects_image (numpy.ndarray): The objects to insert.
+            objects_mask (numpy.ndarray): The mask of the objects.
+            background (numpy.ndarray): The background image.
+            mask (numpy.ndarray): The mask of the background.
+        Returns:
+            tuple: The combined image and mask.
+        """
+        mask = np.where(objects_mask > 0, objects_mask, mask)
+        for c in range(background.shape[0]):
+            background[c] = np.where(objects_mask > 0, objects_image[c], background[c])
+        return background, mask
     
 
     def generate_augmented_sample(self):
+        """
+        Generates an augmented sample by applying object-based augmentation and background augmentation.
+        Returns:
+            tuple: The augmented image and mask.
+        """
         self.flag_object_augm = False
         # -------------------------------------------------------------------
         # find the background
@@ -217,113 +354,6 @@ class Generator:
         return image, mask
     
 
-    def _find_background(self):
-        if len(self.background_list) and random.random() < self.extra_background_prob:
-            print("I should no be in here...")
-            # TODO: not implemented extra backgrounds
-            rnd_choice = random.choice(self.background_list)
-        else:
-            rnd_choice = random.choice(self.file_name_list)
-            with rasterio.open(IMAGES_PATH + "/" + rnd_choice) as src:
-                background = src.read()
-                background = np.nan_to_num(background, nan=0.0)
-
-            mask = self.masks[rnd_choice]["image"][0] # mask has shape (1, 1024, 1024)
-        return background, mask, rnd_choice
-    
-    def _prepare_image_and_mask(self, background, mask):
-        background = background.clone() if torch.is_tensor(background) else background.copy()
-        mask  = mask.clone() if torch.is_tensor(mask) else mask.copy()
-        background = background.numpy() if torch.is_tensor(background) else background
-        mask  = mask.numpy() if torch.is_tensor(mask) else mask
-        return background, mask
-
-    def _apply_object_augmentation(self, name, background, mask):
-        temp_mask = mask.copy()
-        temp_image = np.zeros_like(background)
-        target_ids = [obj[-1] for obj in self.extracted_objects if obj[0] == name]
-        num_of_objects = 0
-
-        if self.extra_objects > 0:
-            print("Adding extra objects")
-            random_num = random.randint(0, self.extra_objects)
-            print(f"Random number of objects: {random_num}")
-            for _ in range(random_num):
-                intersection = True
-                iteration = 0
-
-                while intersection and iteration < 10: # iteration: how mmany attempts to try to add an object
-                    iteration += 1
-
-                    extra_obj = random.choice(self.extracted_objects)
-                    extra_id = extra_obj[-1]
-
-                    if extra_id in target_ids:
-                        # do not duplicate objects
-                        continue
-
-                    cropped_obj, cropped_mask = self.crop_target_object(extra_id)
-
-                    if np.sum(cropped_mask * temp_mask) == 0:
-                        if random.random() < self.object_augm_prob:
-                            # by some probability, augment object
-                            cropped_obj, cropped_mask = self._augment_image_mask(cropped_obj, cropped_mask)
-                            self.flag_object_augm = True
-                            if np.sum(cropped_mask * temp_mask) != 0:
-                                # ensure it does not overlap withany objects
-                                continue
-
-                        intersection = False
-                        temp_mask = np.maximum(cropped_mask, temp_mask)
-                        for c in range(temp_image.shape[0]):
-                            temp_image[c] = np.where(cropped_mask > 0, cropped_obj[c], temp_image[c])
-
-                        target_ids.append(extra_id)
-                        num_of_objects += 1
-            print(f"Added {num_of_objects} objects")
-            temp_mask = np.where(mask > 0, 0, temp_mask)
-        # else:
-        #     # should be an object selected, not image. So this should be changed
-        #     cropped_obj, cropped_mask = self.crop_target_object(target_ids[0]) 
-        return temp_image, temp_mask # returns image and mask of cropped objects
-
-    
-    def _add_shadows(self, img, mask):
-        if mask.ndim == 3:
-            mask = mask[0] # ensure correct dimensions
-
-        shadow_mask = mask.copy()
-
-        shift_x = random.randint(0, 4)
-        shift_y = random.randint(0, 4)
-
-        shifted_mask = np.zeros_like(shadow_mask)
-        h, w = shadow_mask.shape
-        shifted_mask[shift_x:, shift_y:] = shadow_mask[:h - shift_x, :w - shift_y]
-
-        shadow = (shifted_mask > 0) & (mask == 0)
-        
-        # plt.title("Shadow mask")
-        # plt.imshow(shadow)
-        # plt.show()
-
-        alpha = random.choice([0.4, 0.5]) #([0.3, 0.4, 0.45])
-        for idx, _ in enumerate(self.channels_background):
-            img[idx] = np.where(shadow > 0, img[idx] * alpha, img[idx])
-
-        return img # returns objects with shadows
-
-    def _augment_image_mask(self, image, mask):
-        augm_image, augm_mask  = augment(image, mask, self.augm_prob, self.color_augm_prob, self.geometric_augm_prob)
-        return augm_image, augm_mask
-    
-    def _insert_objects_to_background(self, objects_image, objects_mask, background, mask):
-        mask = np.where(objects_mask > 0, objects_mask, mask)
-        for c in range(background.shape[0]):
-            background[c] = np.where(objects_mask > 0, objects_image[c], background[c])
-        return background, mask
-    
-
 # -------------------------------------------------------------------
 #  Create OBA dataset
 # -------------------------------------------------------------------
@@ -343,6 +373,26 @@ def create_OBA_dataset(
         batch_size=1,
         min_area=1000,
         ) -> TensorDataset:
+    """
+    Create a dataset with object-based augmentation (OBA) samples.
+    Args:
+        prob_of_OBA (float): Probability of generating an OBA sample.
+        subset (bool): Whether to use a subset of the data.
+        base_augm (bool): Whether to apply base augmentation.
+        object_augm (bool): Whether to apply object-based augmentation.
+        extra_background_prob (float): Probability of using an extra background.
+        background_augm_prob (float): Probability of augmenting the background.
+        shadows (bool): Whether to add shadows to the objects.
+        extra_objects (int): Number of extra objects to add.
+        object_augm_prob (float): Probability of augmenting the objects.
+        augm_prob (float): Probability of applying augmentation.
+        geometric_augm_prob (float): Probability of applying geometric augmentation.
+        color_augm_prob (float): Probability of applying color augmentation.
+        batch_size (int): Batch size for the dataset.
+        min_area (int): Minimum area for the objects to be considered.
+    Returns:
+        TensorDataset: A dataset containing the augmented samples.
+    """
 
     x_train_dict = load_images(subset=subset)
     y_train_dict = load_masked_images(subset=subset)
