9d4843ce8b9f333ceb06ef638c45ba739be93780
synneremmen <91556819+synneremmen@users.noreply.github.com>
2025-04-25 16:45:34 +0200

    cleanup in train

--- train/train.py
+++ train/train.py
@@ -4,20 +4,16 @@ from tqdm import trange
 
 from torch.amp import GradScaler, autocast
 
-# cuda effeciency
-scaler = GradScaler()  # No arguments needed
-
 def train(n_epochs, optimizer, model, loss_fn, train_loader, device, val_loader=None, scheduler=None):
     n_train_batch = len(train_loader)
     losses_train = []
-    scores_train = []
+
+    # cuda effecient
+    scaler = GradScaler()
 
     if val_loader is not None:
         n_val_batch = len(val_loader)
         losses_val = []
-
-    # model.train()
-    # optimizer.zero_grad()
 
     model = model.to(device)
     
@@ -30,40 +26,35 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, device, val_loader=
             imgs = imgs.to(device)
             labels = labels.to(device)
 
-            optimizer.zero_grad()   # new
-
-            ### commented out to text cuda stuff
+            ### commented out to if not using autocast 
+            # # Normal training
             # outputs = model(imgs)
             # loss = loss_fn(outputs, labels)
             # loss.backward()
             # optimizer.step()
             # optimizer.zero_grad()
 
-             # Mixed precision forward pass
+            # Autocast
+            optimizer.zero_grad()  
             with autocast(device_type='cuda'):
                 outputs = model(imgs)
                 loss = loss_fn(outputs, labels)
-            
-            # Mixed precision backward pass
             scaler.scale(loss).backward()
-
-            # Unscale before clearing memory
-            scaler.unscale_(optimizer)  # 🔹 Added this line before clearing cache
+            scaler.unscale_(optimizer)
             torch.cuda.empty_cache()    
-
             scaler.step(optimizer)
             scaler.update()
-            loss_train += loss.item()
 
-            del imgs, labels, outputs, loss  # Clear memory, new
+
+            loss_train += loss.item()
+            del imgs, labels, outputs, loss  # Clear memory
 
         losses_train.append(loss_train / n_train_batch)
 
         if val_loader is not None:
             model.eval()
-            with torch.inference_mode():    # changed to torch.inference_mode() from torch.no_grad()
+            with torch.inference_mode():    # if not using autocast (?): torch.no_grad()
                 for imgs, labels in val_loader:
-                    # model.eval()
                     imgs = imgs.to(device)
                     labels = labels.to(device)
 
@@ -71,13 +62,12 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, device, val_loader=
                     loss = loss_fn(outputs, labels)
                     loss_val += loss.item()
 
-                    del imgs, labels, outputs, loss  # Clear memory, new
+                    del imgs, labels, outputs, loss  # Clear memory
                     torch.cuda.empty_cache()   # Clear cache
 
                 losses_val.append(loss_val / n_val_batch)
 
-        #   if epoch == 1 or epoch % 5 == 0:
-        # print for every epoch
+        # if epoch == 1 or epoch % 5 == 0:
         print(f'--------- Epoch: {epoch} ---------')
         print('Training loss {:.5f} at {}'.format(loss_train / n_train_batch, datetime.datetime.now()))
         if val_loader is not None:
